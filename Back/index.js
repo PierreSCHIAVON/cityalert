dcc55fdf148a20b686cecd4d5a1dd29c68feeea7
PierreSc <pierre.schiavon@gmail.com>
2025-10-27 14:26:09 +0100

    initialisation swagger

--- Back/index.js
+++ Back/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const setupSwagger = require('./swagger');
+
 const app = express();
 const port = 3000;
 
+// Middleware pour parser le JSON
+app.use(express.json());
+
+// Exemple de route
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Bienvenue sur l’API Alertes Citoyennes 🚨');
 });
 
+// Initialisation de Swagger
+setupSwagger(app);
+
+// Démarrage du serveur
 app.listen(port, () => {
-    console.log(`Serveur Express lancé sur http://localhost:${port}`);
+    console.log(`🚀 Serveur Express lancé sur http://localhost:${port}`);
+    console.log(`📘 Documentation Swagger disponible sur http://localhost:${port}/api-docs`);
 });
+
