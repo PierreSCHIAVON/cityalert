e85707068e15c96fb686aaf0fdc1d2936cc668e8
PierreSc <pierre.schiavon@gmail.com>
2025-11-17 16:10:59 +0100

    fix media

--- Back/api/medias/mediaroutes.js
+++ Back/api/medias/mediaroutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const {
-    getMedia,
-    getMediaById,
-    createMedia,
-    updateMedia,
-    deleteMedia
+    getMedias,
+    getMediasById,
+    createMedias,
+    updateMedias,
+    deleteMedias
 } = require("./mediacontroller");
 
-router.get("/", getMedia);
-router.get("/:id", getMediaById);
-router.post("/", createMedia);
-router.put("/:id", updateMedia);
-router.delete("/:id", deleteMedia);
+router.get("/", getMedias);
+router.get("/:id", getMediasById);
+router.post("/", createMedias);
+router.put("/:id", updateMedias);
+router.delete("/:id", deleteMedias);
 
 module.exports = router;
