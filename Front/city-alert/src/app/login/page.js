27479cea4882abe7d44e6b01082c9c3ecab1b494
DouglasQuashie <idotetudes@gmail.com>
2025-12-08 21:42:08 +0100

    FIX import useAuth

--- Front/city-alert/src/app/login/page.js
+++ Front/city-alert/src/app/login/page.js
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAuth } from './AuthContext';
+import { useAuth } from '../AuthContext';
 import './style.css';
 
 export default function Home() {
